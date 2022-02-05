e77204e304201f5683944dae451c4e99d3ca45e3
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-02-05 09:16:44 -0300

    modificada funcao virarCartas e criada funcao pra checar correspondencia, tambem criada a funcao conferirSeGanhou

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let quantidadeCartas = null;
+const itensBack = ['back1','back2','back3','back4','back5','back6','back7']
+let conferirCartas = []
+let acertos = null
+let primeiraCarta = null
+let segundaCarta = null
+
+
 function jogar(){
     
     quantidadeCartas = parseInt(prompt("digite um numero par de cartas(entre 4 e 14) para jogar "))  
     while((quantidadeCartas % 2 !== 0) | (quantidadeCartas < 4) | (quantidadeCartas > 14)){
         quantidadeCartas = parseInt(prompt("digite um numero par de cartas(entre 4 e 14) para jogar "))
     }
-
+    acertos = quantidadeCartas/2
     alert(quantidadeCartas)
 }
 
@@ -13,7 +20,7 @@ function embaralhar() {
 	return Math.random() - 0.5; 
 }
 
-const itensBack = ['back1','back2','back3','back4','back5','back6','back7']
+
 function gerarCartas(){
     const lista = []
     const listaGeradora = []
@@ -37,7 +44,7 @@ function cartasNaMesa(){
     
     for(let i = 0; i < listaGeradora.length; i++){
         secao.innerHTML +=  `   
-            <section class="card " onclick="virarCartas(this)">
+            <section class="card " onclick="virarCartas(this)" data-card="${listaGeradora[i]}">
                 <div class="card__front"></div>
                 <div class="card__back ${listaGeradora[i]}"></div>
             </section>
@@ -45,10 +52,74 @@ function cartasNaMesa(){
     }
 }
 
-
 function virarCartas(carta) {
-    carta.querySelector(".card__front").classList.toggle("turn__front");
-    carta.querySelector(".card__back").classList.toggle("turn__back");
+    carta.querySelector(".card__front").classList.toggle("turn__front")
+    carta.querySelector(".card__back").classList.toggle("turn__back")
+    carta.classList.add('fixed')
+
+    if(!primeiraCarta){
+        primeiraCarta = carta 
+        return false
+    }
+
+    segundaCarta = carta
+    correspondencia()
+}
+
+function correspondencia(){
+    let ehCorrespondente = primeiraCarta.dataset.card === segundaCarta.dataset.card
+    console.log(ehCorrespondente)
+}
+
+
+
+// function verificaIgualdade(carta){
+    
+//     for(let i=0;i < itensBack.length ;i++){
+//         if (carta.querySelector('.' + itensBack[i]) !== null){
+//             conferirCartas.push(itensBack[i])
+//         }
+//     }
+//     if (conferirCartas.length == 2){
+//         if (conferirCartas[0] !== conferirCartas[1]){
+//             const card1 = document.querySelector(' .'+ conferirCartas[0]).parentNode
+//             card2.classList.remove("fixed")
+            
+//             const card2 = carta.querySelector(' .'+ conferirCartas[1]).parentNode
+//             card2.classList.remove("fixed")
+//             acertos -= 1
+//             //conferirSeGanhou()
+            
+            
+//         }
+//         conferirCartas = []    
+            
+//     }
+    
+//     else{
+//         setTimeout(() => {
+//             let card1 = document.querySelector(' .'+ conferirCartas[0] + '.turn__back')
+//             if (card1 !== null){
+//                 card1 = card1.parentNode
+//                 virarCartas(card1)
+//             }
+//             let card2 = document.querySelector(' .'+ conferirCartas[1] + '.turn__back')
+//             if (card2 !== null){
+//                 card2 = card2.parentNode
+//                 virarCartas(card2)
+//             }
+//         },1000)
+//         //setTimeout(()=>{conferirCartas=[]}, 2000)
+
+
+//     }
+// }
+
+function conferirSeGanhou(){
+    if (acertos === 0){
+        setTimeout( () => {alert('Você ganhou em X jogadas!')}, 1000)
+    }
+
 }
 
 jogar()
